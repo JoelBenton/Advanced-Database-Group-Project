a1b4cd98c82942e5df03690a353f7def4c4011ef
Nicholas <Niko@dipasquali.com>
2025-05-07 17:56:33 +0100

    small colour change

--- mongo-data-api-app/app/(root)/doctor/[id]/page.tsx
+++ mongo-data-api-app/app/(root)/doctor/[id]/page.tsx
@@ -85,7 +85,7 @@ export default function DoctorPage() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 p-8">
+    <div className="min-h-screen bg-[#dbeafe] p-8">
       {/* Header */}
       <div className="flex justify-between items-center mb-8">
         <h1 className="text-3xl font-bold">Appointments Dashboard</h1>
